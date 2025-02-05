ec58198e750d94bef2b22b7ee4f1e3632d3dc11b
xkylz1 <fauzansaputra741@gmail.com>
2025-02-06 03:16:34 +0800

    feat: update main func

--- Server/main.go
+++ Server/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"go-todo-app/config"
+	"go-todo-app/routes"
 )
 
 func main() {
-	router := gin.Default()
-
-	// Simple health check route
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Welcome to Go REST API!"})
-	})
-
-	// Start the server on port 8080
-	router.Run(":8080")
+	config.ConnectDB()
+	r := routes.SetupRouter()
+	r.Run(":8080")
 }
